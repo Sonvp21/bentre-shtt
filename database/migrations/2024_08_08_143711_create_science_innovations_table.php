d1a40515c4e5531c31aba57e9c067c6e33d09431
cuong1211 <cuonghaha43@gmail.com>
2024-08-08 15:02:20 +0700

    test 2

--- database/migrations/2024_08_08_143711_create_science_innovations_table.php
+++ database/migrations/2024_08_08_143711_create_science_innovations_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('science_innovations', function (Blueprint $table) {
             $table->id();
-            
             $table->timestamps();
         });
     }
